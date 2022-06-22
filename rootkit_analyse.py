6a4489ad5726f275ba7570c833b4843c7210cf8e
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-06-22 17:29:06 +0400

    Add files via upload

---
+++ rootkit_analyse.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+os.system ("apt-get install figlet")
+os.system ("clear")
+os.system ("figlet ROOTKIT ANALIZ ")
+
+print("""
+ROOTKIT ANALIZ PROGRAMINA XOS GELMISINIZ :) 
+""")
+os.system("chkrootkit")
