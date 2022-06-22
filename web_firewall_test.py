04e257a16d2747344081c58545598a5c4227f379
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-06-22 15:47:27 +0400

    Add files via upload

---
+++ web_firewall_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+os.system ("apt-get install figlet")
+os.system ("clear")
+os.system ("figlet TEHLUKESIZLIK DIVARI TESBITI ")
+
+print("""
+TEHLUKESIZLIK DIVARI TEST ELEMEK PROGRAMINA XOS GELMISINIZ :) 
+""")
+
+sayt= input("SAYT ADDRESINI YAZIN : ")
+os.system("wafw00f " +sayt)
